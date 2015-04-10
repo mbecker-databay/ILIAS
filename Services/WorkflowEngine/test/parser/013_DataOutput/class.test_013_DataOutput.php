d2d57579212a6667a7433bd8e5c1c120a2eca1b5
mbecker <mbecker@databay.de>
2015-04-10 16:54:57 +0200

    - Testing DataOutput

--- Services/WorkflowEngine/test/parser/013_DataOutput/class.test_013_DataOutput.php
+++ Services/WorkflowEngine/test/parser/013_DataOutput/class.test_013_DataOutput.php
@@ -52,8 +52,33 @@ class test_013_DataOutput extends PHPUnit_Framework_TestCase
 		$this->assertEquals($goldsample, $parse_result, 'Output does not match goldsample.');
 
 		require_once $this->getTestOutputFilename($test_name);
+		/** @var ilBaseWorkflow $process */
 		$process = new $test_name;
-		$this->assertFalse($process->isActive());
+		$process->writeOutputVar('DataOutput_1', 'YaddaYadda');
+		$process->startWorkflow();
+		$all_triggered = true;
+		foreach($process->getNodes() as $node)
+		{
+			/** @var ilNode $node*/
+			foreach($node->getDetectors() as $detector)
+			{
+				/** @var ilSimpleDetector $detector */
+				if(!$detector->getActivated())
+				{
+					$all_triggered = false;
+				}
+			}
+			foreach($node->getEmitters() as $emitter)
+			{
+				/** @var ilActivationEmitter $emitter */
+				if(!$emitter->getActivated())
+				{
+					$all_triggered = false;
+				}
+			}
+		}
+		$this->assertEquals($process->getOutputVars(), array('DataOutput_1' => 'YaddaYadda'), 'Outputvar was not kept.');
+		$this->assertTrue($all_triggered, 'Not all nodes were triggered.');
 
 		unlink($this->getTestOutputFilename($test_name));
 	}
